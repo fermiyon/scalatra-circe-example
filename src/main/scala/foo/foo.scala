c51686990f0cdb482f5df77c633eac0f40782d02
Selman <selman@Selmans-MacBook-Pro.local>
2019-01-22 15:35:53 +0300

    first json

--- src/main/scala/foo/foo.scala
+++ src/main/scala/foo/foo.scala
@@ -12,4 +12,20 @@ class foo extends ScalatraServlet {
     views.html.hello()
   }
 
+  get("/json") {
+    FlowerData.all
+  }
+
+}
+
+object FlowerData {
+  case class Flower(slug: String, name: String)
+
+  /**
+   * Some fake flowers data so we can simulate retrievals.
+   */
+  var all = List(
+      Flower("yellow-tulip", "Yellow Tulip"),
+      Flower("red-rose", "Red Rose"),
+      Flower("black-rose", "Black Rose"))
 }
